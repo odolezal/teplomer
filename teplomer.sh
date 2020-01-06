15b8096963c7860086e822761ca64ca5b598c661
Ondřej Doležal <odolezal@odolezal.cz>
2020-01-06 20:57:32 +0100

    Update teplomer.sh

--- teplomer.sh
+++ teplomer.sh
@@ -1,7 +1,31 @@
+#Integrace se serverem TMEP.cz pro RS232 teplomer Papouch TM.
+#https://github.com/odolezal/teplomer
+#verze: 2.0
+#=====================================
+
 #!/bin/bash
-#Vyctu hodnotu teploty z cidla a ulozim do promene teplota,
-read teplota < /dev/ttyUSB0 && echo -n $teplota | tr -d "\r"
-#nasledne uriznu pismeno C abych dostal jen cislo
-#nakonec poslu na server tmep.cz ktery udela vse ostatni
-curl http://subdomena.tmep.cz/?nazevteplomeru=${teplota::-1}
-#konec
+
+#Casove razitko
+now=$(date +"%H:%M:%S, %d.%m.%Y")
+
+#Subdomena na serveru TMEP.cz
+tmep_subdom="subdomena"
+
+#GUID na serveru TMEP.cz
+tmep_guid="xxxyyyzzz"
+
+#Rozhrani seriove linky (zjistime napr. prikazem "ls -l /dev/tty*")
+serial_port="/dev/ttyUSB0"
+
+#Vyctu hodnotu teploty z cidla a ulozim do promene "teplota".
+read teplota < "$serial_port" && echo -n $teplota | tr -d "\r" &>/dev/null
+
+#Konstrukce "${teplota:2:-1}" urizne pismeno "C" z konce a "+" a "0" ze zacatku textoveho retezce.
+
+#Debug (mozno zakomentovat):
+echo "================================="
+echo "Cas a datum:" "$now"
+echo "Teplota:" "${teplota:2:-1}" "°C"
+
+#Poslu HTTP pozadavek na server TMEP.cz
+curl "http://${tmep_subdom}.tmep.cz/?${tmep_guid}=${teplota::-1}"
